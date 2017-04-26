0a1fe577d959476ba2f5ab8d11ee262c8d112a13
ramkumar_tomrain <ramkumar@tomrain.com>
2017-04-27 04:32:28 +0530

    unit testing

--- src/app/product-detail/product-detail.component.spec.ts
+++ src/app/product-detail/product-detail.component.spec.ts
@@ -1,13 +1,25 @@
+import { ActivatedRoute } from '@angular/router';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { ProductDetailComponent } from './product-detail.component';
+import { Product } from '../Product';
+import { ProductService } from '../product.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Observable } from 'rxjs';
+import { MockBackend } from '@angular/http/testing';
+
+
+let MockProduct: Product = <Product>{id: 1, name: 'Superman',src: 'new',info: 'hai how are you', price: 100};
 
 describe('ProductDetailComponent', () => {
   let component: ProductDetailComponent;
   let fixture: ComponentFixture<ProductDetailComponent>;
+  let productService: ProductService;
+  let activatedRoute: ActivatedRoute;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      providers:[ProductService],
       declarations: [ ProductDetailComponent, NavbarComponent ]
     })
     .compileComponents();
@@ -18,6 +30,7 @@ describe('ProductDetailComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
+
 
   it('should create', () => {
     expect(component).toBeTruthy();
