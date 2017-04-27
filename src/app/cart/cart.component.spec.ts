f1d3b365091c05ba6e50564c2c79e77dfd3dca72
Mohanlal <t349445@cs.ad.klmcorp.net>
2017-04-27 14:49:56 +0530

    cart and login controller testing

--- src/app/cart/cart.component.spec.ts
+++ src/app/cart/cart.component.spec.ts
@@ -1,14 +1,31 @@
+import { CartComponent } from './cart.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NavbarComponent } from '../navbar/navbar.component';
-import { CartComponent } from './cart.component';
+import { ProductService } from '../product.service';
+import { MockBackend } from '@angular/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { BaseRequestOptions, Http } from '@angular/http';
 
 describe('CartComponent', () => {
   let component: CartComponent;
   let fixture: ComponentFixture<CartComponent>;
+  let productService: ProductService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CartComponent, NavbarComponent]
+    providers: [
+        ProductService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
+          deps: [ MockBackend, BaseRequestOptions ]
+        }
+      ],
+      declarations: [ CartComponent, NavbarComponent],
+      imports:[RouterTestingModule]
     })
     .compileComponents();
   }));
@@ -17,6 +34,7 @@ describe('CartComponent', () => {
     fixture = TestBed.createComponent(CartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    productService = TestBed.get(ProductService);
   });
 
   it('should create', () => {
--- src/app/my-new-component/my-new-component.component.spec.ts
+++ src/app/my-new-component/my-new-component.component.spec.ts
@@ -1,14 +1,39 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoginComponent } from './my-new-component.component';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {AuthenticationService, User} from '../my-new-service.service';
+import { MockBackend } from '@angular/http/testing';
+import { BaseRequestOptions, Http } from '@angular/http';
+import { RouterTestingModule } from '@angular/router/testing';
+
+let MockProduct: User = <User>{email: "vasanths@live.in", password: 'Superman'};
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let authService: AuthenticationService;
+
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      providers: [
+        AuthenticationService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
+          deps: [ MockBackend, BaseRequestOptions ]
+        }
+      ],
+      declarations: [
+        LoginComponent
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
@@ -17,6 +42,7 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    authService = TestBed.get(AuthenticationService);
   });
 
   it('should create', () => {
