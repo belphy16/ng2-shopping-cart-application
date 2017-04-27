d4934e032764549c956022eea3a0141af6b2696e
Mohanlal <t349445@cs.ad.klmcorp.net>
2017-04-27 13:14:10 +0530

    product component testing

--- src/app/private-component/private-component.component.spec.ts
+++ src/app/private-component/private-component.component.spec.ts
@@ -1,14 +1,43 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { NavbarComponent } from '../navbar/navbar.component';
 import { ProductComponent } from './private-component.component';
+import { ProductService } from '../product.service';
+import { MockBackend } from '@angular/http/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Router } from '@angular/router';
+import { BaseRequestOptions, Http } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { Product } from '../Product';
+
+
+let MockProduct: Product = <Product>{id: 1, name: 'Superman',src: 'new',info: 'hai how are you', price: 100};
+
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
   let fixture: ComponentFixture<ProductComponent>;
+  let productService: ProductService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductComponent, NavbarComponent ]
+providers: [
+        ProductService,
+        MockBackend,
+        BaseRequestOptions,
+        {
+          provide: Http,
+          useFactory: (backend: MockBackend, options: BaseRequestOptions) => new Http(backend, options),
+          deps: [ MockBackend, BaseRequestOptions ]
+        }
+      ],
+      declarations: [
+        ProductComponent,
+        NavbarComponent
+      ],
+      imports: [
+        FormsModule,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
   }));
@@ -17,6 +46,7 @@ describe('ProductComponent', () => {
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    productService = TestBed.get(ProductService);
   });
 
   it('should create', () => {
